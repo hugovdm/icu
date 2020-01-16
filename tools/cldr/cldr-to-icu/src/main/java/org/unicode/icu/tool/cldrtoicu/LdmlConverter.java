20c0fd1608b4fce0902faa3768ea9e2451fd8c1b
Hugo van der Merwe <hugovdm@gmail.com>
2020-01-16 16:13:48 +0100

    PR: https://github.com/sffc/icu/pull/4
    Original Commit: e156a903f73722729c861389213089a8f2f25f90
    
    Early-return from ProcessLdml if it's not going to do anything.

--- tools/cldr/cldr-to-icu/src/main/java/org/unicode/icu/tool/cldrtoicu/LdmlConverter.java
+++ tools/cldr/cldr-to-icu/src/main/java/org/unicode/icu/tool/cldrtoicu/LdmlConverter.java
@@ -283,6 +283,10 @@ public final class LdmlConverter {
 
         String cldrVersion = config.getCldrVersion();
 
+        if (splitDirs.isEmpty()) {
+          return;
+        }
+
         Map<IcuLocaleDir, DependencyGraph> graphMetadata = new HashMap<>();
         splitDirs.forEach(d -> graphMetadata.put(d, new DependencyGraph(cldrVersion)));
 
