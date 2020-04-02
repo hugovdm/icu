743835faed90be2acfb22d8de33328807ced2097
Hugo van der Merwe <17109322+hugovdm@users.noreply.github.com>
2020-04-03 01:16:08 +0200

    checkUnitsState DEBUGGING OUTPUT.

--- icu4c/source/i18n/unitconverter.cpp
+++ icu4c/source/i18n/unitconverter.cpp
@@ -34,16 +34,35 @@ MeasureUnit extractBaseUnit(const MeasureUnit &source,
                             UErrorCode &status) {
     MeasureUnit result;
     int32_t count;
+    fprintf(stderr, "initial status: %s, splitting: %s\n", u_errorName(status), source.getIdentifier());
     const auto singleUnits = source.splitToSingleUnits(count, status);
+    fprintf(stderr, "singleUnits count: %d\n", count);
+    if (U_FAILURE(status)) {
+        fprintf(stderr, "FAILED: splitToSingleUnits(<%s>) - %s\n", source.getIdentifier(),
+                u_errorName(status));
+    }
     if (U_FAILURE(status)) return result;
 
     for (int i = 0; i < count; ++i) {
         const auto &singleUnit = singleUnits[i];
+        fprintf(stderr, "singleUnits[%d]: %s\n", i, singleUnit.getIdentifier());
         // Extract `ConversionRateInfo` using the absolute unit. For example: in case of `square-meter`,
         // we will use `meter`
+
+        if (U_FAILURE(status)) {
+            fprintf(stderr, "calling forMeasureUnit %s\n", singleUnit.getIdentifier());
+        }
         const auto singleUnitImpl = SingleUnitImpl::forMeasureUnit(singleUnit, status);
+        fprintf(stderr, "singleUnitImpl: %s\n", singleUnitImpl.build(status).getIdentifier());
+        if (U_FAILURE(status)) {
+            fprintf(stderr, "extractConversionInfo failure: %s\n", u_errorName(status));
+        }
         const auto &rateInfo =
             extractConversionInfo(singleUnitImpl.identifier, conversionRateInfoList, status);
+        fprintf(stderr, "rateInfo.baseUnit: %s\n", rateInfo.baseUnit.data());
+        if (U_FAILURE(status)) {
+            fprintf(stderr, "extractConversionInfo failure: %s\n", u_errorName(status));
+        }
         if (U_FAILURE(status)) return result;
 
         // Multiply the power of the singleUnit by the power of the baseUnit. For example, square-hectare
@@ -52,7 +71,10 @@ MeasureUnit extractBaseUnit(const MeasureUnit &source,
         auto singleBaseUnit = SingleUnitImpl::forMeasureUnit(baseUnit, status);
         singleBaseUnit.dimensionality *= singleUnit.getDimensionality(status);
 
+        fprintf(stderr, "result: <%s> * <%s> ->", result.getIdentifier(),
+                singleBaseUnit.build(status).getIdentifier());
         result = result.product(singleBaseUnit.build(status), status);
+        fprintf(stderr, "<%s>\n", result.getIdentifier());
     }
 
     return result;
@@ -63,8 +85,11 @@ MeasureUnit extractBaseUnit(const MeasureUnit &source,
 UnitsMatchingState U_I18N_API
 checkUnitsState(const MeasureUnit &source, const MeasureUnit &target,
                 const MaybeStackVector<ConversionRateInfo> &conversionRateInfoList, UErrorCode &status) {
+    fprintf(stderr, "\nInitial: %s - checkUnitsState(%s)\n", u_errorName(status), source.getIdentifier());
     auto sourceBaseUnit = extractBaseUnit(source, conversionRateInfoList, status);
+    fprintf(stderr, "sourceBaseUnit: %s - %s\n", sourceBaseUnit.getIdentifier(), u_errorName(status));
     auto targetBaseUnit = extractBaseUnit(target, conversionRateInfoList, status);
+    fprintf(stderr, "targetBaseUnit: %s - %s\n", sourceBaseUnit.getIdentifier(), u_errorName(status));
 
     if (U_FAILURE(status)) return UNCONVERTIBLE;
 
